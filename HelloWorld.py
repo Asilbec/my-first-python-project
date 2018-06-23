cf3bcde3345c3e2858f6882c962d7c7c03d1e57a
Asilbec <40507855+Asilbec@users.noreply.github.com>
2018-06-22 21:12:22 -0400

    uploaded file

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+
+age = int(input("ENTER NUMBER HERE: "))
+if age <= 21:
+    print("GTFO")
+if age > 100:
+    print("Too gold")
+if age > 21 and age < 100:
+    print("Pay money ")
+    health =input ("Do you have a health condition?: ")
+    if health == "yes":
+        print("Boi if dont get yo goofy head out of here ")
+    if health == "no":
+        money = int(input("How much you willing to spend?:"))
+        if money <= 100:
+            print("The cheapest thing we have costs 101 boi get yo broke ass out of here")
+        if money > 101 and money <1000:
+            print("ehh you commoner")
+        if money > 1000:
+            print("right this way sir")
+
+
